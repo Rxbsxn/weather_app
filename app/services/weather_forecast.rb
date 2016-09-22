8a7a3a87f8c00c9d967531c4ed6fc8f572022c4a
Pawel Kuwik <czikarito94@gmail.com>
2016-09-22 18:45:17 +0200

    showing weather params

--- app/services/weather_forecast.rb
+++ app/services/weather_forecast.rb
@@ -1,18 +1,36 @@
 require 'open_weather'
-
 class WeatherForecast < OpenWeather::Current
   def self.for_city(name)
     Info.new(city("#{name}, PL", options))
   end
 
   def self.options
-    { units: 'metric', APPID: 'cdbaa06b52336ef3d5647654c0388d92' }
+    { lang: 'pl', units: 'metric', APPID: 'cdbaa06b52336ef3d5647654c0388d92' }
   end
 
   class Info < OpenStruct
-    def temp_min
-      main["temp_min"]
+    def temp
+      main['temp']
+    end
+
+    def humidity
+      main['humidity']
+    end
+
+    def sunrise
+      Time.zone.at(sys['sunrise'])
+    end
+
+    def sunset
+      Time.zone.at(sys['sunset'])
+    end
+
+    def pressure
+      main['pressure']
+    end
+
+    def speed
+      wind['speed']
     end
   end
-  #attributes :id, :name, :temp_min, :temp_max, :country
 end
